fd2180473c86b65557cbbb1a8b5dc5177653ff5d
Dan Dumitriu <dandumitriu33@gmail.com>
2022-09-11 09:13:32 +0300

    Add initial commit - helloworld

---
+++ helloworld/go.mod
@@ -0,0 +1,3 @@
+module github.com/dandumitriu33/go-testing/helloworld
+
+go 1.18
---
+++ helloworld/helloworld.go
@@ -0,0 +1,12 @@
+// First package in the testing series.
+package main
+
+import "fmt"
+
+func Hello() string {
+	return "Hello, world"
+}
+
+func main() {
+	fmt.Println(Hello())
+}
