9975eabc642a624070c1f9de2311e847622d9fab
Dan Dumitriu <dandumitriu33@gmail.com>
2022-09-12 08:44:11 +0300

    Add TDD Spanish version code

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -3,15 +3,20 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Christian"))
+	fmt.Println(Hello("Christian", ""))
 }
