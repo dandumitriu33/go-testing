e30fbe4e1051a4418951e2afc23348196fcba8f2
Dan Dumitriu <dandumitriu33@gmail.com>
2022-09-16 08:42:31 +0300

    Add table tests example

--- structsmethodsinterfaces/structsmethodsinterfaces_test.go
+++ structsmethodsinterfaces/structsmethodsinterfaces_test.go
@@ -14,6 +14,25 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+
+}
+
+func TestArea2(t *testing.T) {
+
 	checkArea := func(t testing.TB, shape Shape, want float64) {
 		t.Helper()
 		got := shape.Area()
