0c1773a5ec7073af6efbb3f89899b019a4f8d502
Dan Dumitriu <dandumitriu33@gmail.com>
2022-09-27 08:42:09 +0300

    Add Select statement in new Racer func

--- selecting/selecting.go
+++ selecting/selecting.go
@@ -6,6 +6,24 @@ import (
 )
 
 func Racer(a, b string) (winner string) {
+	select {
+	case <-ping(a):
+		return a
+	case <-ping(b):
+		return b
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
+
+func Racer2(a, b string) (winner string) {
 	aDuration := measureResponseTime(a)
 	bDuration := measureResponseTime(b)
 
