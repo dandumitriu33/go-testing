8227674095ed499adfb2cec57ea3e72d7df4dd61
Dan Dumitriu <dandumitriu33@gmail.com>
2022-09-29 09:27:34 +0300

    Add reflection test for slices with several refactoring options

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -15,22 +15,62 @@ type Profile struct {
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
+	// Different refactor
+	numberOfValues := 0
+	var getField func(int) reflect.Value
 
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
-		}
-		// if field.Kind() == reflect.String {
-		// 	fn(field.String())
-		// }
-		// if field.Kind() == reflect.Struct {
-		// 	walk(field.Interface(), fn)
-		// }
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		numberOfValues = val.NumField()
+		getField = val.Field
+	case reflect.Slice:
+		numberOfValues = val.Len()
+		getField = val.Index
 	}
+
+	for i := 0; i < numberOfValues; i++ {
+		walk(getField(i).Interface(), fn)
+	}
+
+	// // Classic refactor
+	// switch val.Kind() {
+	// case reflect.Struct:
+	// 	for i := 0; i < val.NumField(); i++ {
+	// 		walk(val.Field(i).Interface(), fn)
+	// 	}
+	// case reflect.Slice:
+	// 	for i := 0; i < val.Len(); i++ {
+	// 		walk(val.Index(i).Interface(), fn)
+	// 	}
+	// case reflect.String:
+	// 	fn(val.String())
+	// }
+
+	// if val.Kind() == reflect.Slice {
+	// 	for i := 0; i < val.Len(); i++ {
+	// 		walk(val.Index(i).Interface(), fn)
+	// 	}
+	// 	return
+	// }
+
+	// for i := 0; i < val.NumField(); i++ {
+	// 	field := val.Field(i)
+
+	// 	switch field.Kind() {
+	// 	case reflect.String:
+	// 		fn(field.String())
+	// 	case reflect.Struct:
+	// 		walk(field.Interface(), fn)
+	// 	}
+	// 	// if field.Kind() == reflect.String {
+	// 	// 	fn(field.String())
+	// 	// }
+	// 	// if field.Kind() == reflect.Struct {
+	// 	// 	walk(field.Interface(), fn)
+	// 	// }
+	// }
 }
 
 func getValue(x interface{}) reflect.Value {
