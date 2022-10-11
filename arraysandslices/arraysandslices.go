32cfaf8b37d03a874b6f17a64f6a0e029448a18f
Dan Dumitriu <dandumitriu33@gmail.com>
2022-10-11 08:24:23 +0300

    Add accumulator refactor on Sum and SumAllTails

--- arraysandslices/arraysandslices.go
+++ arraysandslices/arraysandslices.go
@@ -1,12 +1,18 @@
 package arraysandslices
 
+// Sum calculates the total from a slice of numbers.
 func Sum(numbers []int) int {
-	sum := 0
-	for _, number := range numbers {
-		sum += number
-	}
-	return sum
+	add := func(acc, x int) int { return acc + x }
+	return Reduce(numbers, add, 0)
 }
+
+// func Sum(numbers []int) int {
+// 	sum := 0
+// 	for _, number := range numbers {
+// 		sum += number
+// 	}
+// 	return sum
+// }
 
 func SumAll(numbersToSum ...[]int) []int {
 	var sums []int
@@ -16,18 +22,32 @@ func SumAll(numbersToSum ...[]int) []int {
 	return sums
 }
 
-func SumAllTails(numbersToSum ...[]int) []int {
-	var sums []int
-	for _, numbers := range numbersToSum {
-		if len(numbers) == 0 {
-			sums = append(sums, 0)
+// SumAllTails calculates the sums of all but the first
+// number given a collection of slices.
+func SumAllTails(numbers ...[]int) []int {
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
 		} else {
-			tail := numbers[1:]
-			sums = append(sums, Sum(tail))
+			tail := x[1:]
+			return append(acc, Sum(tail))
 		}
 	}
-	return sums
+	return Reduce(numbers, sumTail, []int{})
 }
+
+// func SumAllTails(numbersToSum ...[]int) []int {
+// 	var sums []int
+// 	for _, numbers := range numbersToSum {
+// 		if len(numbers) == 0 {
+// 			sums = append(sums, 0)
+// 		} else {
+// 			tail := numbers[1:]
+// 			sums = append(sums, Sum(tail))
+// 		}
+// 	}
+// 	return sums
+// }
 
 func Reduce[A any](collection []A, accumulator func(A, A) A, initialValue A) A {
 	var result = initialValue
