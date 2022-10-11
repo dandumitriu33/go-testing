35a6dfb0648ff1278c20f7402f5e10780dcd55be
Dan Dumitriu <dandumitriu33@gmail.com>
2022-10-11 08:17:10 +0300

    Add slices initial Reduce function

--- arraysandslices/arraysandslices.go
+++ arraysandslices/arraysandslices.go
@@ -28,3 +28,11 @@ func SumAllTails(numbersToSum ...[]int) []int {
 	}
 	return sums
 }
+
+func Reduce[A any](collection []A, accumulator func(A, A) A, initialValue A) A {
+	var result = initialValue
+	for _, x := range collection {
+		result = accumulator(result, x)
+	}
+	return result
+}
