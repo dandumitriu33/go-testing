630dfd077612bd56056d62cdaa000789c9672131
Dan Dumitriu <dandumitriu33@gmail.com>
2022-10-12 08:54:02 +0300

    Add bank code improvement with Account and test

--- arraysandslices/arraysandslices_test.go
+++ arraysandslices/arraysandslices_test.go
@@ -6,22 +6,47 @@ import (
 )
 
 func TestBadBank(t *testing.T) {
-	transactions := []Transaction{
-		{
-			From: "Chris",
-			To:   "Riya",
-			Sum:  100,
-		},
-		{
-			From: "Adil",
-			To:   "Chris",
-			Sum:  25,
-		},
-	}
 
-	AssertEqual(t, BalanceFor(transactions, "Riya"), 100)
-	AssertEqual(t, BalanceFor(transactions, "Chris"), -75)
-	AssertEqual(t, BalanceFor(transactions, "Adil"), -25)
+	t.Run("check balance is equal simple", func(t *testing.T) {
+		transactions := []Transaction{
+			{
+				From: "Chris",
+				To:   "Riya",
+				Sum:  100,
+			},
+			{
+				From: "Adil",
+				To:   "Chris",
+				Sum:  25,
+			},
+		}
+
+		AssertEqual(t, BalanceFor(transactions, "Riya"), 100)
+		AssertEqual(t, BalanceFor(transactions, "Chris"), -75)
+		AssertEqual(t, BalanceFor(transactions, "Adil"), -25)
+	})
+
+	t.Run("improved bank code balance equality", func(t *testing.T) {
+		var (
+			riya  = Account{Name: "Riya", Balance: 100}
+			chris = Account{Name: "Chris", Balance: 75}
+			adil  = Account{Name: "Adil", Balance: 200}
+
+			transactions = []Transaction{
+				NewTransaction(chris, riya, 100),
+				NewTransaction(adil, chris, 25),
+			}
+		)
+
+		newBalanceFor := func(account Account) float64 {
+			return NewBalanceFor(account, transactions).Balance
+		}
+
+		AssertEqual(t, newBalanceFor(riya), 200)
+		AssertEqual(t, newBalanceFor(chris), 0)
+		AssertEqual(t, newBalanceFor(adil), 175)
+	})
+
 }
 
 func TestSum(t *testing.T) {
