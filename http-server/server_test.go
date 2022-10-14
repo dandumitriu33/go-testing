e0c92affaa15eb8c0fd92666bdbeb757f0babe24
Dan Dumitriu <dandumitriu33@gmail.com>
2022-10-14 09:11:21 +0300

    TDD 1 - test - check correct player name on winCalls - not hardcoded 'Bob'

--- http-server/server_test.go
+++ http-server/server_test.go
@@ -77,15 +77,20 @@ func TestStoreWins(t *testing.T) {
 	// 	assertStatus(t, response.Code, http.StatusAccepted)
 	// })
 
-	t.Run("it records wins when POST", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodPost, "/players/Pepper", nil)
+	t.Run("it records wins on POST", func(t *testing.T) {
+		player := "Pepper"
+
+		request := newPostWinRequest(player)
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
 
 		assertStatus(t, response.Code, http.StatusAccepted)
 		if len(store.winCalls) != 1 {
-			t.Errorf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+			t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+		}
+		if store.winCalls[0] != player {
+			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], player)
 		}
 	})
 }
