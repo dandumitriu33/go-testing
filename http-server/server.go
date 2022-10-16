fabf24fe716df21689cca7478f9140aeb9580c93
Dan Dumitriu <dandumitriu33@gmail.com>
2022-10-16 20:14:16 +0300

    TDD 2 - make it pass - add router

--- http-server/server.go
+++ http-server/server.go
@@ -16,27 +16,20 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
-
-	switch r.Method {
-	case http.MethodPost:
-		p.processWin(w, player)
-	case http.MethodGet:
-		p.showScore(w, player)
-	}
-
-	// if r.Method == http.MethodPost {
-	// 	w.WriteHeader(http.StatusAccepted)
-	// 	return
-	// }
-
-	// player := strings.TrimPrefix(r.URL.Path, "/players/")
-	// score := p.store.GetPlayerScore(player)
-	// if score == 0 {
-	// 	w.WriteHeader(http.StatusNotFound)
-	// }
-
-	// fmt.Fprint(w, score)
+	router := http.NewServeMux()
+	router.Handle("/league", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	router.Handle("/players/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		player := strings.TrimPrefix(r.URL.Path, "/players/")
+		switch r.Method {
+		case http.MethodPost:
+			p.processWin(w, player)
+		case http.MethodGet:
+			p.showScore(w, player)
+		}
+	}))
+	router.ServeHTTP(w, r)
 }
 
 func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
