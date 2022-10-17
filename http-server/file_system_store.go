2315436e5c6179f1475cb1e40e0ce630e08ba8d3
Dan Dumitriu <dandumitriu33@gmail.com>
2022-10-17 09:20:59 +0300

    TDD 3 - refactor - add ReadSeeker for offset reset on reading

--- http-server/file_system_store.go
+++ http-server/file_system_store.go
@@ -5,12 +5,13 @@ import (
 )
 
 type FileSystemPlayerStore struct {
-	database io.Reader
+	database io.ReadSeeker
 }
 
 func (f *FileSystemPlayerStore) GetLeague() []Player {
 	// var league []Player
 	// json.NewDecoder(f.database).Decode(&league)
+	f.database.Seek(0, 0)
 	league, _ := NewLeague(f.database)
 	return league
 }
