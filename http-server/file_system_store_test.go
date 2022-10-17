51a81b05de9fd4232b2dc2843a9b4562ca590afb
Dan Dumitriu <dandumitriu33@gmail.com>
2022-10-17 09:42:31 +0300

    TDD 3 - refactor - add createTempFile for file system DB test

--- http-server/file_system_store_test.go
+++ http-server/file_system_store_test.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	"strings"
+	"io"
+	"os"
 	"testing"
 )
 
 func TestFileSystemStore(t *testing.T) {
 
 	t.Run("league from a reader", func(t *testing.T) {
-		database := strings.NewReader(`[
+		// database := strings.NewReader(`[
+		// 	{"Name": "Cleo", "Wins": 10},
+		// 	{"Name": "Chris", "Wins": 33}]`)
+		database, cleanDatabase := createTempFile(t, `[
 			{"Name": "Cleo", "Wins": 10},
 			{"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
 
 		store := FileSystemPlayerStore{database}
 
@@ -28,9 +33,13 @@ func TestFileSystemStore(t *testing.T) {
 	})
 
 	t.Run("get player score", func(t *testing.T) {
-		database := strings.NewReader(`[
+		// database := strings.NewReader(`[
+		// 	{"Name": "Cleo", "Wins": 10},
+		// 	{"Name": "Chris", "Wins": 33}]`)
+		database, cleanDatabase := createTempFile(t, `[
 			{"Name": "Cleo", "Wins": 10},
 			{"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
 
 		store := FileSystemPlayerStore{database}
 
@@ -38,6 +47,7 @@ func TestFileSystemStore(t *testing.T) {
 		want := 33
 		assertScoreEquals(t, got, want)
 	})
+
 }
 
 func assertScoreEquals(t testing.TB, got, want int) {
@@ -45,4 +55,22 @@ func assertScoreEquals(t testing.TB, got, want int) {
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
 	}
+}
+
+func createTempFile(t testing.TB, initialData string) (io.ReadWriteSeeker, func()) {
+	t.Helper()
+
+	tmpfile, err := os.CreateTemp("", "db")
+
+	if err != nil {
+		t.Fatalf("could not create temp file %v", err)
+	}
+
+	tmpfile.Write([]byte(initialData))
+	removeFile := func() {
+		tmpfile.Close()
+		os.Remove(tmpfile.Name())
+	}
+
+	return tmpfile, removeFile
 }
